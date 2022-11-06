86559d5d10520852a83b777ff775cec588fbc038
Danila Serookiy <dan.serookiy@mail.ru>
2022-11-06 22:44:47 +0300

    Initial commit

---
+++ src/main/java/by/serookiy/finaltask/repositories/BooksRepository.java
@@ -0,0 +1,12 @@
+package by.serookiy.finaltask.repositories;
+
+import by.serookiy.finaltask.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
+}
